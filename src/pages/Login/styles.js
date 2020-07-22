276493d36aa61b457f7fe8fab11e33ff787cda4a
Talles Marques <tallesemanuel1@gmail.com>
2020-07-22 15:20:26 -0300

    Responsividade final

--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,5 +1,6 @@
 import { makeStyles } from '@material-ui/core'
 import ThemeLogo from '../../assets/theme-logo.png';
+import ThemeLogoMenor from '../../assets/theme-logo-menor.png';
 
 export const useStyles = makeStyles(theme => ({
   root: {
@@ -25,7 +26,7 @@ export const useStyles = makeStyles(theme => ({
       left: "calc(50% - 624px/2)",
 
       width: "624px",
-      height: "471px",
+      // height: "471px",
     },
     [theme.breakpoints.down('sm')]: { //Só funciona abaixo dos 960px (sm) de largura
       top: "5vh",
@@ -33,55 +34,66 @@ export const useStyles = makeStyles(theme => ({
 
       width: "90vw",
       // height: "60vh",
-    }
-
+    },
   },
 
+
+
   loginTxt: {
-    position: "relative",
-    top: "77px",
-    bottom: "71.33%",
 
     color: "#FFFFFF",
     fontFamily: "Roboto, sans-serif",
     fontStyle: "normal",
     fontWeight: "normal",
     fontSize: "32px",
-    lineHeight: "37px",
+    // lineHeight: "37px",
 
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    textAlign: "center"
+    textAlign: "center",
+    // position: "relative",
+    // top: "77px",
+    // bottom: "71.33%",
+
+    // [theme.breakpoints.up('sm')]: { //Só funciona acima dos 960px (sm) de largura 
+    // },
+    // [theme.breakpoints.down('sm')]: { //Só funciona abaixo dos 960px (sm) de largura
+    //   // position: "inherit",
+
+    margin: "40px 0",
+    // }
   },
 
   loginLogo: {
-    backgroundImage: `url(${ThemeLogo})`,
 
     position: "absolute",
     [theme.breakpoints.up('sm')]: { //Só funciona acima dos 960px (sm) de largura 
       width: "210px",
       height: "140px",
-      left: "10px"
+      left: "10px",
+      backgroundImage: `url(${ThemeLogo})`,
     },
     [theme.breakpoints.down('sm')]: { //Só funciona abaixo dos 960px (sm) de largura
-      width: "154px",
-      height: "93px",
-      left: "calc(50% - 154px/2)",
-      bottom: "40px"
+      width: "180px",
+      height: "120px",
+      left: "calc(50% - 180px/2)",
+      bottom: "40px",
+      backgroundImage: `url(${ThemeLogoMenor})`,
     }
   },
 
   emaill: {
     backgroundColor: "#FFFFFF",
     borderRadius: "5px",
-    marginTop: "130px",
 
     [theme.breakpoints.up('sm')]: { //Só funciona acima dos 960px (sm) de largura 
+      // marginTop: "130px",
       marginLeft: "35px",
       width: "550px"
     },
     [theme.breakpoints.down('sm')]: { //Só funciona abaixo dos 960px (sm) de largura
+      marginTop: "0px",
       marginLeft: "5%",
       width: "90%"
     }
@@ -113,7 +125,11 @@ export const useStyles = makeStyles(theme => ({
     textAlign: "left",
     color: "#FFFFFF",
 
-    marginTop: "25px",
+    "&:hover": {
+      textDecoration: "underline"
+    },
+
+    marginTop: "10px",
 
     [theme.breakpoints.up('sm')]: { //Só funciona acima dos 960px (sm) de largura 
       fontSize: "20px",
@@ -121,7 +137,7 @@ export const useStyles = makeStyles(theme => ({
     },
     [theme.breakpoints.down('sm')]: { //Só funciona abaixo dos 960px (sm) de largura
       fontSize: "16px",
-      marginLeft: "20px",
+      marginLeft: "6%",
     }
   },
 
@@ -136,7 +152,7 @@ export const useStyles = makeStyles(theme => ({
     boxSizing: "border-box",
     borderRadius: "2px",
 
-    marginTop: "25px",
+    marginTop: "10px",
 
     fontFamily: "Roboto",
     fontStyle: "normal",
@@ -150,14 +166,15 @@ export const useStyles = makeStyles(theme => ({
     cursor: "pointer",
 
     [theme.breakpoints.up('sm')]: { //Só funciona acima dos 960px (sm) de largura 
+      height: "60px",
       width: "552px",
-      height: "73px",
 
+      marginBottom: "30px",
       marginLeft: "35px",
     },
     [theme.breakpoints.down('sm')]: { //Só funciona abaixo dos 960px (sm) de largura
       width: "90%",
-      height: "50px",
+      height: "56px",
 
       marginBottom: "30px",
       marginLeft: "5%"

ad502c4b4914bc71b3bc3871fc2c3e17c558f5b4
João Rivello <jmprivello@gmail.com>
2020-07-23 20:12:13 -0300

    menu lateral "cru"
    falta por as opções

--- src/pages/Dashboard/menu.js
+++ src/pages/Dashboard/menu.js
@@ -1,72 +1,217 @@
 import React from 'react';
-import { CssBaseline } from "@material-ui/core"
+import { Link } from "react-router-dom"
+import { createBrowserHistory } from "history";
+
 import clsx from 'clsx';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import { useStyles } from './styles'
-import { FiMenu } from "react-icons/fi"
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import HomeIcon from '@material-ui/icons/Home';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import AddIcon from '@material-ui/icons/Add';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
 
-export default function Menu() {
+const drawerWidth = 240;
 
-    const classes = useStyles();
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    backgroundColor: '#2D64F3',
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    // backgroundColor: '#FE2121',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    backgroundColor: '#FE2121',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    // padding: theme.spacing(3),
+  },
+  headerInfos: {
+      width: "100%",
+      justifyContent: "space-between",
+      display: "flex",
+      flexDirection: "row"
+  },
+  users: {
+    display: "flex",
+    flexDirection: "row"
+  },
+  user: {
+    marginTop: "4%",
+    marginRight: "2%",
+  }
+}));
 
-    const [state, setState] = React.useState({
-        left: false,
-    });
+export default function MiniDrawer(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-    const toggleDrawer = (anchor, open) => (event) => {
-        if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-            return;
-        }
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
-        setState({ state, [anchor]: open });
-    };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
-    const list = (anchor) => (
-        <div
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <div className={classes.headerInfos}>
+          <div className={classes.mainTitle}>
+          <Typography variant="h6" noWrap>
+            Paraíso das Bombas
+          </Typography>
+          </div>
+          <div className={classes.users}>
+          <AccountCircleOutlinedIcon className={classes.user} />
+          <Typography variant="h6" className={classes.username}>Admin</Typography>
+          </div>
+          </div>
+        </Toolbar>
+      </AppBar>
+        <Drawer
+          variant="permanent"
+          className={clsx(classes.drawer, {
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open,
+            }),
+          }}
         >
-            <List>
-                {['Cadastro de Cliente',
-                    'Cadastro de Funcionário',
-                    'Lista de Clientes',
-                    'Cadastro de Equipamentos',
-                    'Lista de Equipamentos',
-                    'Cliente X Equipamento'].map((text) => (
-                        <ListItem button key={text}>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-            </List>
-        </div>
-    );
+          <div className={classes.toolbar}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            </IconButton>
+          </div>
+          <Divider />
+          <Divider />
+          <List>
+            <ListItem
+              button
+              component={Link}
+              to=""
+            >
+              <ListItemIcon><HomeIcon /></ListItemIcon>
+              <ListItemText>Home</ListItemText>
+            </ListItem>
+            
+            <ListItem
+              button
+              component={Link}
+              to=""
+            >
+              <ListItemIcon><PersonAddIcon /></ListItemIcon>
+              <ListItemText>Cadastrar Usuário</ListItemText>
+            </ListItem>
 
-    return (
-        <div>
-             <div className={classes.appbar}>
-                {['left'].map((anchor) => (
-                    <React.Fragment key={anchor}>
-                        <CssBaseline />
-                        <Button
-                            onClick={toggleDrawer(anchor, true)}>
-                            <FiMenu size={24} color="#FFFFFF" className={classes.icon} />
-                        </Button>
-                        <SwipeableDrawer
-                            anchor={anchor}
-                            open={state[anchor]}
-                            onClose={toggleDrawer(anchor, false)}
-                            onOpen={toggleDrawer(anchor, true)}
-                        >
-                            {list(anchor)}
-                        </SwipeableDrawer>
-                    </React.Fragment>
-                ))}
-            </div>
-        </div>
-    )
+            <ListItem
+              button
+              component={Link}
+              to=""
+            >
+              <ListItemIcon><AddIcon /></ListItemIcon>
+              <ListItemText>Cadastrar Produto</ListItemText>
+            </ListItem>
+            
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+    
+          {props.children}
+          {console.log('renderizei')}
+
+        </main>
+    </div>
+  );
 }
+
+
+

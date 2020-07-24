483c21f16317a2b4a9fb06b31b9191c478f67b2f
Talles Marques <tallesemanuel1@gmail.com>
2020-07-24 14:27:13 -0300

    Adição de useStates primarios ao gráfico

--- src/pages/Dashboard/dashboard.js
+++ src/pages/Dashboard/dashboard.js
@@ -4,7 +4,73 @@ import Menu from './menu';
 import Graphic from './chart';
 import { useEffect, useState } from 'react';
 
+import { clientTemp, modelTemp, equipTemp, dataTemp } from './temp';
+
 export default function Dashboard() {
+  const [situation, setSituation] = useState({
+    ok: Number,
+    revisao: Number,
+    atencao: Number
+  });
+
+  const [values, setValues] = useState([])
+  const [limits, setLimits] = useState([])
+  const [totalEquipment, setTotalEquipment] = useState();
+
+  useEffect(() => { //total de equipamentos
+    const quantidade = clientTemp.client.equipment;
+
+    setTotalEquipment(quantidade);
+  }, []);
+
+  useEffect(() => { //seta valores 
+    const data = dataTemp.data;
+
+    const valuesData = data.map(equipment => {
+      return {
+        temperature: equipment.temperature,
+        current: equipment.current,
+        voltage: equipment.voltage
+      }
+    })
+
+    setValues(valuesData)
+  }, [])
+
+  useEffect(() => { //seta limites
+    const data = modelTemp.data;
+
+    const limitsData = data.map(model => {
+      return {
+        temperatureLimit: model.temperatureLimit,
+        currentLimit: model.currentLimit,
+        voltageLimit: model.voltageLimit
+      }
+    })
+
+    setLimits(limitsData)
+  }, [])
+
+  useEffect(() => { //configura situações
+    // const limitTemp = 20;
+    // const valueTemp = 18;
+
+    let numOk = 0;
+    let numAtencao = 0;
+    let numRevisao = 0;
+
+    numOk++;
+    numAtencao++;
+
+    const situationData = {
+      ok: numOk,
+      revisao: numRevisao,
+      atencao: numAtencao
+    };
+
+    setSituation(situationData);
+  }, [])
+
 
   const classes = useStyles();
 
@@ -17,13 +83,22 @@ export default function Dashboard() {
             </div>
       <div className={classes.graphic}>
         <div className={classes.graphic1}>
-          <Graphic data={[50, 50]} colors={['red', "gray"]} labels={["Revisão"]} />
+          <Graphic
+            data={[situation.revisao, totalEquipment - situation.revisao]}
+            colors={['red', "gray"]}
+            labels={["Revisão"]} />
         </div>
         <div className={classes.graphic1}>
-          <Graphic data={[50, 50]} colors={['yellow', "gray"]} labels={["Atenção"]} />
+          <Graphic
+            data={[situation.atencao, totalEquipment - situation.atencao]}
+            colors={['yellow', "gray"]}
+            labels={["Atenção"]} />
         </div>
         <div className={classes.graphic1}>
-          <Graphic data={[40, 60]} colors={['green', "gray"]} labels={["OK"]} />
+          <Graphic
+            data={[situation.ok, totalEquipment - situation.ok]}
+            colors={['green', "gray"]}
+            labels={["OK"]} />
         </div>
       </div>
     </div >

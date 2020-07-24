ce76ced23d120fc9f4c46a5a4ab09ab36e7c33aa
Talles Marques <tallesemanuel1@gmail.com>
2020-07-24 11:23:24 -0300

    comentários de suporte

--- src/pages/Dashboard/dashboard.js
+++ src/pages/Dashboard/dashboard.js
@@ -2,29 +2,30 @@ import React from 'react';
 import { useStyles } from './styles';
 import Menu from './menu';
 import Graphic from './chart';
+import { useEffect, useState } from 'react';
 
 export default function Dashboard() {
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <Menu />
-            <div className={classes.sidebar}></div>
-            <div className={classes.tittle}>
-                Situação das Bombas
+  return (
+    <div className={classes.root}>
+      <Menu />
+      <div className={classes.sidebar}></div>
+      <div className={classes.tittle}>
+        Situação das Bombas
             </div>
-            <div className={classes.graphic}>
-                <div className={classes.graphic1}>
-                    <Graphic data={[10, 90]} colors={['red', "gray"]} labels={["Revisão"]} />
-                </div>
-                <div className={classes.graphic1}>
-                    <Graphic data={[50, 50]} colors={['yellow', "gray"]} labels={["Atenção"]} />
-                </div>
-                <div className={classes.graphic1}>
-                    <Graphic data={[40, 60]} colors={['green', "gray"]} labels={["OK"]} />
-                </div>
-            </div>
-        </div >
-    )
+      <div className={classes.graphic}>
+        <div className={classes.graphic1}>
+          <Graphic data={[50, 50]} colors={['red', "gray"]} labels={["Revisão"]} />
+        </div>
+        <div className={classes.graphic1}>
+          <Graphic data={[50, 50]} colors={['yellow', "gray"]} labels={["Atenção"]} />
+        </div>
+        <div className={classes.graphic1}>
+          <Graphic data={[40, 60]} colors={['green', "gray"]} labels={["OK"]} />
+        </div>
+      </div>
+    </div >
+  )
 }

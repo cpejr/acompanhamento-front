87f3996459b47e3b577cd1f9bc14d2d8c4e0b245
Talles Marques <tallesemanuel1@gmail.com>
2020-08-07 16:06:30 -0300

    Fixing

--- src/pages/CadastroEquipamento/cadastroEquipamentoStyle.js
+++ src/pages/CadastroEquipamento/cadastroEquipamentoStyle.js
@@ -64,7 +64,7 @@ export const useStyles = makeStyles(theme => ({
   },
 
   containerForm: {
-    padding: "40px 60px",
+    padding: "20px 60px 40px",
 
     display: "flex",
     flexDirection: "column",

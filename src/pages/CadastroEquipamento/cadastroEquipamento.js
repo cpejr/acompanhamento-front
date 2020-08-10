a08d8949e23c41d3084c3703605aca14b907beba
Talles Marques <tallesemanuel1@gmail.com>
2020-08-10 17:02:05 -0300

    Bug do Enter e input com autocomplete

--- src/pages/CadastroEquipamento/cadastroEquipamento.js
+++ src/pages/CadastroEquipamento/cadastroEquipamento.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import {
   CssBaseline,
@@ -31,7 +31,41 @@ export default function CadastroEquipamento(props) {
     alert("Submit")
   }
 
-  useEffect(() => { console.log(formData) }, [formData])
+  // useEffect(() => { console.log(formData) }, [formData])
+
+  const numeroSerieRef = useRef(null);
+  const limitTempRef = useRef(null);
+  const limitCorrRef = useRef(null);
+  const limitTensRef = useRef(null);
+  const cpfRef = useRef(null);
+  const buttonSubmitRef = useRef(null);
+
+  const relacionamentosRef = [
+    { name: "numeroSerie", ref: limitTempRef },
+    { name: "limiteTemperatura", ref: limitCorrRef },
+    { name: "limiteCorrente", ref: limitTensRef },
+    { name: "limiteTensao", ref: cpfRef },
+    { name: "cpf", ref: buttonSubmitRef }
+  ]
+
+  function doNothing(event) {
+    var keyCode = event.keyCode ? event.keyCode : event.which ? event.which : event.charCode;
+    if (keyCode === 13) {
+
+      var e = window.event;
+      e.cancelBubble = true;
+      e.returnValue = false;
+
+      if (e.stopPropagation) {
+        e.stopPropagation();
+        e.preventDefault();
+      }
+
+      relacionamentosRef.find(referencia =>
+        (referencia.name === event.target.name)
+      ).ref.current.focus();
+    }
+  }
 
   return (
     <React.Fragment>
@@ -50,52 +84,63 @@ export default function CadastroEquipamento(props) {
         <form className={classes.form} onSubmit={() => handleSubmit("cadastroEquip")}>
           <div className={classes.containerForm}>
             <TextField
-              className={classes.inputs}
               name="numeroSerie"
+              className={classes.inputs}
               value={formData.numeroSerie}
               onChange={handleChangeInput}
               label="Número da série"
               type="text"
               helperText="*Obrigatório"
-              variant="filled" />
+              variant="filled"
+              autoComplete="off"
+              autoFocus
+              inputRef={numeroSerieRef} onKeyPress={doNothing} />
             <TextField
-              className={classes.inputs}
               name="limiteTemperatura"
+              className={classes.inputs}
               value={formData.limiteTemperatura}
               onChange={handleChangeInput}
               label="Limite TEMPERATURA"
               type="text"
               helperText="*Obrigatório"
-              variant="filled" />
+              variant="filled"
+              autoComplete="off"
+              inputRef={limitTempRef} onKeyPress={doNothing} />
             <TextField
-              className={classes.inputs}
               name="limiteCorrente"
+              className={classes.inputs}
               value={formData.limiteCorrente}
               onChange={handleChangeInput}
               label="Limite CORRENTE"
               type="text"
               helperText="*Obrigatório"
-              variant="filled" />
+              variant="filled"
+              autoComplete="off"
+              inputRef={limitCorrRef} onKeyPress={doNothing} />
             <TextField
-              className={classes.inputs}
               name="limiteTensao"
+              className={classes.inputs}
               value={formData.limiteTensao}
               onChange={handleChangeInput}
               label="Limite TENSÃO"
               type="text"
               helperText="*Obrigatório"
-              variant="filled" />
+              variant="filled"
+              autoComplete="off"
+              inputRef={limitTensRef} onKeyPress={doNothing} />
             <TextField
-              className={classes.inputs}
               name="cpf"
+              className={classes.inputs}
               value={formData.cpf}
               onChange={handleChangeInput}
               label="CPF"
               type="text"
               helperText="*Obrigatório"
-              variant="filled" />
+              variant="filled"
+              inputRef={cpfRef} onKeyPress={doNothing} />
             <div>
-              <Button type="submit" className={classes.botaocadastrar}>Cadastrar</Button>
+              <Button type="submit" ref={buttonSubmitRef}
+                className={classes.botaocadastrar}>Cadastrar</Button>
             </div>
           </div>
         </form>

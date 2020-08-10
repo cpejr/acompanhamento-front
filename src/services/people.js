1c2321a6088265bda645d745a811afe2860274c0
Antonio Britto <antonio.britto.2000@hotmail.com>
2020-08-10 12:43:44 -0300

    elementos ficticios para compor a lista de pessoas

---
+++ src/services/people.js
@@ -0,0 +1,39 @@
+const People = [
+    {
+        id: "15",
+        funcao: "Administrador",
+        lastactive: "10/08/2020",
+        name: "Antonio Britto",
+        birth: "07/07/2000",
+        adress: "blablabla",
+        cpf: "515151511", //CPF unico
+        email: "jsabduasud",
+        phone: "98989898",
+    },
+
+    {
+        id: "17",
+        funcao: "Funcionário",
+        lastactive: "01/08/2020",
+        name: "Maiara",
+        birth: "50/50/05",
+        adress: "blablabla",
+        cpf: "515151511", //CPF unico
+        email: "jsabduasud",
+        phone: "98989898",
+    },
+
+    {
+        id: "16",
+        funcao: "Cliente",
+        lastactive: "05/08/2020",
+        name: "João Rivello",
+        birth: "02/02/2000",
+        adress: "blablabla",
+        cpf: "515151511", //CPF unico
+        email: "jsabduasud",
+        phone: "98989898",
+    },
+]
+
+export default People;

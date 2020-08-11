34b522298991f409a5bc77aa9f5294b9655844bb
Antonio Britto <antonio.britto.2000@hotmail.com>
2020-08-10 21:41:31 -0300

    passando dados ficticios criados no tabela.js para uma file separada

--- src/services/people.js
+++ src/services/people.js
@@ -1,39 +1,105 @@
-const People = [
-    {
-        id: "15",
-        funcao: "Administrador",
-        lastactive: "10/08/2020",
-        name: "Antonio Britto",
-        birth: "07/07/2000",
-        adress: "blablabla",
-        cpf: "515151511", //CPF unico
-        email: "jsabduasud",
-        phone: "98989898",
-    },
+const CreatePeople = {
+    "people": [
+        {
+            id: "15",
+            funcao: "Administrador",
+            lastactive: "15/08/2019",
+            name: "Maiara",
+            birth: "07/07/2000",
+            adress: "blablabla",
+            cpf: "515151511", //CPF unico
+            email: "jsabduasud",
+            phone: "98989898",
+        },
+        {
+            id: "15",
+            funcao: "Administrador",
+            lastactive: "15/07/2008",
+            name: "Antonio",
+            birth: "5555555",
+            adress: "blablabla",
+            cpf: "515151511", //CPF unico
+            email: "jsabduasud",
+            phone: "98989898",
+        },
+        {
+            id: "15",
+            funcao: "Cliente",
+            lastactive: "27/02/2020",
+            name: "Juliana",
+            birth: "1111111",
+            adress: "blablabla",
+            cpf: "515151511", //CPF unico
+            email: "jsabduasud",
+            phone: "98989898",
+        },
+        {
+            id: "15",
+            funcao: "Funcionário",
+            lastactive: "10/08/2020",
+            name: "Arthur",
+            birth: "07/07/2000",
+            adress: "blablabla",
+            cpf: "515151511", //CPF unico
+            email: "jsabduasud",
+            phone: "98989898",
+        },
+        {
+            id: "15",
+            funcao: "Cliente",
+            lastactive: "10/08/2020",
+            name: "Joao",
+            birth: "07/07/2000",
+            adress: "blablabla",
+            cpf: "515151511", //CPF unico
+            email: "jsabduasud",
+            phone: "98989898",
+        },
+        {
+            id: "15",
+            funcao: "Administrador",
+            lastactive: "10/08/2020",
+            name: "Jota",
+            birth: "51515151",
+            adress: "blablabla",
+            cpf: "515151511", //CPF unico
+            email: "jsabduasud",
+            phone: "98989898",
+        },
+        {
+            id: "17",
+            funcao: "Funcionário",
+            lastactive: "01/08/2020",
+            name: "Izabela",
+            birth: "50/50/05",
+            adress: "blablabla",
+            cpf: "515151511", //CPF unico
+            email: "jsabduasud",
+            phone: "98989898",
+        },
+        {
+            id: "15",
+            funcao: "Cliente",
+            lastactive: "10/08/2020",
+            name: "Talles",
+            birth: "07/07/8844",
+            adress: "blablabla",
+            cpf: "515151511", //CPF unico
+            email: "jsabduasud",
+            phone: "98989898",
+        },
+        {
+            id: "16",
+            funcao: "Administrador",
+            lastactive: "05/08/2020",
+            name: "Pedro",
+            birth: "02/02/2000",
+            adress: "blablabla",
+            cpf: "515151511", //CPF unico
+            email: "jsabduasud",
+            phone: "98989898",
+        },
+    ]
+}
 
-    {
-        id: "17",
-        funcao: "Funcionário",
-        lastactive: "01/08/2020",
-        name: "Maiara",
-        birth: "50/50/05",
-        adress: "blablabla",
-        cpf: "515151511", //CPF unico
-        email: "jsabduasud",
-        phone: "98989898",
-    },
-
-    {
-        id: "16",
-        funcao: "Cliente",
-        lastactive: "05/08/2020",
-        name: "João Rivello",
-        birth: "02/02/2000",
-        adress: "blablabla",
-        cpf: "515151511", //CPF unico
-        email: "jsabduasud",
-        phone: "98989898",
-    },
-]
-
-export default People;
+export default CreatePeople;

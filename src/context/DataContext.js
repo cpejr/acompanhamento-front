6f00e56e3e31a07cb684f5f5ae50ca57ce8a138d
Talles Marques <tallesemanuel1@gmail.com>
2020-09-22 21:04:48 -0300

    Dados de equipamentos de acordo com o que o axios faria

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,7 +1,7 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { Backdrop, makeStyles, CircularProgress } from '@material-ui/core'
 import CreatePeople from '../services/people';
-import DATA from '../services/data';
+import backend from '../services/backend';
 
 const useStyles = makeStyles((theme) => ({
   backdrop: {
@@ -15,9 +15,28 @@ const DataContext = createContext();
 function DataContextProvider({ children }) {
   const classes = useStyles();
 
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [usersList] = useState(CreatePeople.people);
-  const [equipmentsList] = useState(DATA);
+  const [equipmentsList, setEquipmentsList] = useState([{}]);
+
+  useEffect(() => {
+    (async () => {
+      await backend.getEquipments()
+        .then(data => {
+          const equipments = data.equipment
+          setTimeout(() => {
+            setEquipmentsList(equipments);
+          }, 700);
+        })
+    }
+    )();
+  }, [])
+
+  useEffect(() => {
+    if (equipmentsList[0].client)
+      setLoading(false);
+    else setLoading(true);
+  }, [equipmentsList])
 
   if (loading) { //pagina de carregamento
     return (
--- src/services/Datas/equipments.js
+++ src/services/Datas/equipments.js
@@ -119,5 +119,3 @@ export default {
     }
   ]
 }
-
-export default DATA;

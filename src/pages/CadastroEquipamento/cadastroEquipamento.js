d5022beef54e79cd02a42784f5f4891cbca8d682
Talles Marques <tallesemanuel1@gmail.com>
2020-10-05 21:20:39 -0300

    Arrumando get de modelo

--- src/pages/CadastroEquipamento/cadastroEquipamento.js
+++ src/pages/CadastroEquipamento/cadastroEquipamento.js
@@ -60,21 +60,7 @@ export default function CadastroEquipamento(props) {
         setModels(models);
       })
       .catch(error => {
-        if (error.response) {
-          // Request made and server responded
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-          setOpenMensage(({ open: true, message: "Error 501: Falha no cadastro", type: 'error', time: 5000 }));
-        } else if (error.request) {
-          // The request was made but no response was received
-          console.log(error.request);
-          setOpenMensage(({ open: true, message: "Error 501: Falha no cadastro", type: 'error', time: 5000 }));
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log('Error', error.message);
-          setOpenMensage(({ open: true, message: "Error 501: Falha no cadastro", type: 'error', time: 5000 }));
-        }
+        console.log(error);
         setOpenMensage(({ open: true, message: `Error 504: ${error.message}`, type: 'error', time: 5000 }));
       });
     setLoading(false)

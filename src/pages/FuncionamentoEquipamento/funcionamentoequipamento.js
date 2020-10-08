613a0a9410fddae003fd7c0c35a8295fa402c435
Talles Marques <tallesemanuel1@gmail.com>
2020-10-07 21:11:12 -0300

    Layout inicial da página de detalhes de equipamento

--- src/pages/FuncionamentoEquipamento/funcionamentoequipamento.js
+++ src/pages/FuncionamentoEquipamento/funcionamentoequipamento.js
@@ -1,8 +1,41 @@
-import React, { useState } from 'react';
-import { Link, useParams } from "react-router-dom"
+import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import api from '../../services/api';
+import {
+  CssBaseline,
+  Grid
+} from '@material-ui/core';
+import { useStyles } from './funcionamentoequipamentoStyle'
 
 export default function FuncionamentoEquipamento() {
-return (
-    <h1>Oi pagina</h1>
-)
+  const classes = useStyles();
+  const { id } = useParams()
+
+  const [equipmentData, setEquipmentData] = useState([]);
+
+  // useEffect(() => {
+  //   api.get('')
+  // }, []);
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Grid container>
+        <Grid container className={classes.chartContainer}>
+          <Grid item md="9" className={classes.chart}>
+            Gráfico
+          </Grid>
+          <Grid item md="3" className={classes.chartTable}>
+            Tabela do gráfico
+          </Grid>
+          <Grid item md="12" className={classes.chartButtons}>
+            Mudar gráfico
+          </Grid>
+        </Grid>
+        <Grid item md="12" className={classes.table}>
+          Tabela do equipamento
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
--- src/pages/FuncionamentoEquipamento/funcionamentoequipamentoStyle.js
+++ src/pages/FuncionamentoEquipamento/funcionamentoequipamentoStyle.js
@@ -1,19 +1,36 @@
 import { makeStyles } from '@material-ui/core';
 
 export const useStyles = makeStyles(theme => ({
-    root: {
-        display: "flex",
-        flexDirection: "column",
-        width: "90%",
-        padding: "50px 0 66px 50px",
-        paddingTop: "50px",
-        paddingRight: "0px",
-        paddingBottom: "66px",
-        paddingLeft: "50px",
-        [theme.breakpoints.only("xs")]: {
-            padding: "30px 5% 30px",
-            width: "100%",
-        },
+  root: {
+    boxSizing: "border-box",
+    display: "flex",
+    width: "90%",
+    padding: "50px 0 66px 50px",
+    [theme.breakpoints.only("xs")]: {
+      padding: "30px 5% 30px",
+      width: "100%",
     },
-
+  },
+  chartContainer: {
+    width: "100%",
+    marginBottom: "35px",
+  },
+  chart: {
+    height: "300px",
+    background: "green"
+  },
+  chartTable: {
+    height: "300px",
+    background: "olive"
+  },
+  chartButtons: {
+    height: "20px",
+    background: "blue"
+  },
+  table: {
+    background: "red",
+    width: "100%",
+    marginBottom: "35px",
+    height: "100px"
+  },
 }))

9b4cf68aabc0cbd28e7280fd3c23565a14caca27
Talles Marques <tallesemanuel1@gmail.com>
2020-10-13 22:46:46 -0300

    Preparando para ir ao heroku

--- src/pages/TestEquipment/testEquipment.js
+++ src/pages/TestEquipment/testEquipment.js
@@ -5,26 +5,40 @@ import { useStyles } from './testEquipmentStyle';
 
 export default function TestEquipment() {
 
-    const [testEquipments, setTestEquipments] = useState();
-    const classes = useStyles();
+  const [testEquipments, setTestEquipments] = useState([]);
+  const classes = useStyles();
 
-    useEffect(() => {
-        api.get('testequipment/index')
-            .then(testequipment => {
-                const equipments = testequipment.data.data
-                setTestEquipments(equipments);
-            })
-            .catch(err => {
-                console.error("Backend is not working properly", err);
-            });
-    }, [])
+  useEffect(() => {
+    // setTestEquipments([
+    //   { temperature: 33 },
+    //   { temperature: 33 },
+    //   { temperature: 33 },
+    //   { temperature: 33 },
+    //   { temperature: 33 },
+    //   { temperature: 33 },
+    //   { temperature: 33 },
+    // ]);
+    api.get('testequipment/index')
+      .then(testequipment => {
+        const equipments = testequipment.data.testEquipment
+        setTestEquipments(equipments);
+      })
+      .catch(err => {
+        console.error("Backend is not working properly", err);
+      });
+  }, [])
 
-    return (
-        <div>
-            <div className={classes.root}>
-                Dados teste coletados:
-                {testEquipments}
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className={classes.root}>
+        Dados teste coletados:
+        <ul>
+          {testEquipments.map(equipment => (
+            <li key={equipment.temperature}>{JSON.stringify(equipment)}</li>
+          ))
+          }
+        </ul>
+      </div>
+    </div>
+  )
 }

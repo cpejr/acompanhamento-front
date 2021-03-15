f89f02e1adfa6b1a837c5984efe1838e4cd09025
izabelabrant <izabelabrant@dcc.ufmg.br>
2021-03-15 13:34:19 -0300

    Alterando rota back

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://bombastesteback.herokuapp.com/' //Inserir a URL do backend
+  baseURL: 'http://localhost:3333/' //Inserir a URL do backend
 })
 
 export default api

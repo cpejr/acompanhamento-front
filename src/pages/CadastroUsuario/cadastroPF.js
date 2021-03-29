5e0f906f32909227ab6939710a104c53b712e9f9
Bryan Azevedo <bryan42damasceno@gmail.com>
2021-03-29 09:01:50 -0300

    ADD requisição de pessoa física

--- src/pages/CadastroUsuario/cadastroPF.js
+++ src/pages/CadastroUsuario/cadastroPF.js
@@ -1,71 +1,79 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from "react";
 import {
   TextField,
   FormControlLabel,
   Checkbox,
   Grid,
   useMediaQuery,
-  Button
-} from '@material-ui/core';
-
-import { useStyles } from './cadastroUsuarioStyle';
-import nextInput from '../../services/nextInput';
+  Button,
+} from "@material-ui/core";
+import api from "../../services/api";
+import { useStyles } from "./cadastroUsuarioStyle";
+import nextInput from "../../services/nextInput";
 
 function CadastroPF(props) {
-  const { formData, handleChangeCheck, handleChangeInput, handleSubmit } = props;
+  const {
+    formData,
+    handleChangeCheck,
+    handleChangeInput,
+    handleSubmit,
+  } = props;
 
   const classes = useStyles();
 
-  const nomeRef = useRef(null);
-  const cpfRef = useRef(null);
-  const nascimentoRef = useRef(null);
-  const telefoneRef = useRef(null);
-  const emailRef = useRef(null);
-  const emailConfirmarRef = useRef(null);
-  const senhaRef = useRef(null);
-  const senhaConfirmarRef = useRef(null);
-  const emailPromocionalRef = useRef(null);
-  const buttonRef = useRef(null);
+  const [name, setName] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [birth, setBirth] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [emailConfirm, setEmailConfirm] = useState("");
+  const [senha, setSenha] = useState("");
+  const [senhaConfirm, setSenhaConfirm] = useState("");
 
-  const relacionamentosRef = [
-    { name: "nome", ref: cpfRef },
-    { name: "cpf", ref: nascimentoRef },
-    { name: "nascimento", ref: telefoneRef },
-    { name: "telefone", ref: emailRef },
-    { name: "email", ref: emailConfirmarRef },
-    { name: "emailConfirmar", ref: senhaRef },
-    { name: "senha", ref: senhaConfirmarRef },
-    { name: "senhaConfirmar", ref: emailPromocionalRef },
-    { name: "emailPromocional", ref: buttonRef }
-  ];
+  async function handleRegister() {
+    const data = {
+      name: name,
+      birthdate: birth,
+      cpf: cpf,
+      email: email,
+      number: number,
+      password: senha,
+    };
+
+    try {
+      console.log("OPA", data);
+      const response = await api.post("/user", data);
+      alert(`Você foi cadastrado com sucesso.`);
+    } catch (err) {
+      alert.error("Teve um erro no cadastro, tente novamente.");
+    }
+  }
 
   return (
     <div>
       <form onSubmit={() => handleSubmit("cadastroPF")}>
-        <Grid container spacing={useMediaQuery('(min-width:960px)') ? 5 : 0}>
-          <Grid item xs={12} md={6} >
+        <Grid container spacing={useMediaQuery("(min-width:960px)") ? 5 : 0}>
+          <Grid item xs={12} md={6}>
             <TextField
               name="nome"
               className={classes.inputForm}
               value={formData.nome}
-              onChange={handleChangeInput}
               label="Nome Completo"
               type="text"
               helperText="*Obrigatório"
               variant="filled"
-              inputRef={nomeRef} onKeyPress={e => nextInput(e, relacionamentosRef)}
+              onChange={(e) => setName(e.target.value)}
             />
 
             <TextField
               name="cpf"
               className={classes.inputForm}
               value={formData.cpf}
-              onChange={handleChangeInput}
               label="CPF"
               type="text"
               helperText="*Obrigatório"
               variant="filled"
-              inputRef={cpfRef} onKeyPress={e => nextInput(e, relacionamentosRef)}
+              onChange={(e) => setCpf(e.target.value)}
             />
 
             <TextField
@@ -74,49 +82,43 @@ function CadastroPF(props) {
               label="Data de Nascimento"
               defaultValue="2017-05-24"
               value={formData.nascimento}
-              onChange={handleChangeInput}
               helperText="(Opcional)"
               variant="filled"
               type="date"
-              inputRef={nascimentoRef} onKeyPress={e => nextInput(e, relacionamentosRef)}
+              onChange={(e) => setBirth(e.target.value)}
             />
 
             <TextField
               name="telefone"
               className={classes.inputForm}
               value={formData.telefone}
-              onChange={handleChangeInput}
               label="Número de telefone"
               type="number"
               helperText="*Obrigatório"
               variant="filled"
-              inputRef={telefoneRef} onKeyPress={e => nextInput(e, relacionamentosRef)}
+              onChange={(e) => setNumber(e.target.value)}
             />
-
-
           </Grid>
-          <Grid item xs={12} md={6} >
+          <Grid item xs={12} md={6}>
             <TextField
               name="email"
               className={classes.inputForm}
               value={formData.email}
-              onChange={handleChangeInput}
               label="Endereço de e-mail"
               type="email"
               helperText="*Obrigatório"
               variant="filled"
-              inputRef={emailRef} onKeyPress={e => nextInput(e, relacionamentosRef)}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <TextField
               name="emailConfirmar"
               className={classes.inputForm}
               value={formData.emailConfirmar}
-              onChange={handleChangeInput}
               label="Confirmar e-mail"
               type="email"
               helperText="*Obrigatório"
               variant="filled"
-              inputRef={emailConfirmarRef} onKeyPress={e => nextInput(e, relacionamentosRef)}
+              onChange={(e) => setEmailConfirm(e.target.value)}
             />
 
             <TextField
@@ -124,12 +126,11 @@ function CadastroPF(props) {
               autoComplete="off"
               className={classes.inputForm}
               value={formData.senha}
-              onChange={handleChangeInput}
               label="Criar senha"
               type="password"
               helperText="*Obrigatório"
               variant="filled"
-              inputRef={senhaRef} onKeyPress={e => nextInput(e, relacionamentosRef)}
+              onChange={(e) => setSenha(e.target.value)}
             />
 
             <TextField
@@ -137,12 +138,11 @@ function CadastroPF(props) {
               autoComplete="off"
               className={classes.inputForm}
               value={formData.senhaConfirmar}
-              onChange={handleChangeInput}
               label="Confirmar senha"
               type="password"
               helperText="*Obrigatório"
               variant="filled"
-              inputRef={senhaConfirmarRef} onKeyPress={e => nextInput(e, relacionamentosRef)}
+              onChange={(e) => setSenhaConfirm(e.target.value)}
             />
 
             <FormControlLabel
@@ -152,15 +152,22 @@ function CadastroPF(props) {
                   name="emailPromocional"
                   checked={formData.emailPromocional}
                   onChange={handleChangeCheck}
-                  color="primary" size="small"
-                  inputRef={emailPromocionalRef}
-                  onKeyPress={e => nextInput(e, relacionamentosRef)}
+                  color="primary"
+                  size="small"
                 />
               }
-              label="Desejo receber emails promocionais" />
+              label="Desejo receber emails promocionais"
+            />
           </Grid>
           <Grid item xs={12}>
-            <Button type="submit" ref={buttonRef} className={classes.buttonRegister}>Cadastrar</Button>
+            <Button
+              type="submit"
+              ref={buttonRef}
+              className={classes.buttonRegister}
+              onClick={handleRegister}
+            >
+              Cadastrar
+            </Button>
           </Grid>
         </Grid>
       </form>

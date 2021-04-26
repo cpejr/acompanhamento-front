7eda0ab92545246a691cc97ad46a8d9d7de185d2
Bryan Azevedo <bryan42damasceno@gmail.com>
2021-04-26 19:24:28 -0300

    BUG context

--- src/pages/AtualizacaoUsuario/atualizacaoUsuario.js
+++ src/pages/AtualizacaoUsuario/atualizacaoUsuario.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import {
   CssBaseline,
   Paper,
@@ -12,23 +12,21 @@ import {
   DialogActions,
   Typography,
   Snackbar,
-  CircularProgress
-} from "@material-ui/core"
-import { useParams } from 'react-router';
+  CircularProgress,
+} from "@material-ui/core";
+import { useParams } from "react-router";
 import MuiAlert from "@material-ui/lab/Alert";
 
-import { useStyles } from './atualizacaoUsuarioStyle'
-import users from '../../services/people'
-import { AuthContext } from '../../context/AuthContext';
+import { useStyles } from "./atualizacaoUsuarioStyle";
+import users from "../../services/people";
+import { AuthContext } from "../../context/AuthContext";
 import CadastroPF from "../CadastroUsuario/cadastroPF";
 import CadastroFuncionario from "../CadastroUsuario/cadastroFuncionario";
 import CadastroPJ from "../CadastroUsuario/cadastroPJ";
 import api from "../../services/api";
-import { RssFeed } from '@material-ui/icons';
+import { RssFeed } from "@material-ui/icons";
 
-
-function AtualizacaoUsuario() {
-
+function AtualizacaoUsuario(props) {
   const { id } = useParams();
   const { user } = useContext(AuthContext);
 
@@ -39,8 +37,8 @@ function AtualizacaoUsuario() {
 
   // variaveis do snackbar
   const [openSnackbar, setOpenSnackbar] = useState(false);
-  const [messageSnackbar, setMessageSnackbar] = useState('');
-  const [typeSnackbar, setTypeSnackbar] = useState('info');
+  const [messageSnackbar, setMessageSnackbar] = useState("");
+  const [typeSnackbar, setTypeSnackbar] = useState("info");
   const [loading, setLoading] = useState(false);
 
   const classes = useStyles({ updating });
@@ -57,17 +55,31 @@ function AtualizacaoUsuario() {
   // }, [id, user])
 
   // pega os dados do usuário com o id
+
   useEffect(() => {
-     api
-      .get(`/user/${id}`)
-      .then((response) => {
-        setUserData(response.data.user);
-        setUserDataOriginal(response.data.user);
-      })
-      .catch((error) => {
-        console.warn(error);
-        alert("Erro ao buscar funcionários");
-      })
+    if (!props.perfil) {
+      api
+        .get(`/user/${id}`)
+        .then((response) => {
+          setUserData(response.data.user);
+          setUserDataOriginal(response.data.user);
+        })
+        .catch((error) => {
+          console.warn(error);
+          alert("Erro ao buscar funcionários");
+        });
+    } else {
+      api
+        .get(`/user/${props.userPerfil.id}`)
+        .then((response) => {
+          setUserData(response.data.user);
+          setUserDataOriginal(response.data.user);
+        })
+        .catch((error) => {
+          console.warn(error);
+          alert("Erro ao buscar funcionários");
+        });
+    }
   }, [id]);
 
   function handleChangeInput(event) {
@@ -76,11 +88,10 @@ function AtualizacaoUsuario() {
   }
 
   async function handleSubmit() {
-
-    if (!updating) setUpdating(true)
+    if (!updating) setUpdating(true);
     else {
       setLoading(true);
-      
+
       try {
         const updatedFields = {
           name: userData.name,
@@ -88,66 +99,64 @@ function AtualizacaoUsuario() {
           phonenumber: userData.phonenumber,
           address: userData.address,
           zipcode: userData.zipcode,
-        }
+        };
 
-        if ( 
-          updatedFields.name !== '' &&
-          updatedFields.birthdate !== '' &&
-          updatedFields.phonenumber !== '' &&
-          updatedFields.address !== '' &&
-          updatedFields.zipcode !== '' 
+        if (
+          updatedFields.name !== "" &&
+          updatedFields.birthdate !== "" &&
+          updatedFields.phonenumber !== "" &&
+          updatedFields.address !== "" &&
+          updatedFields.zipcode !== ""
         ) {
           const response = await api.put(`/users/${id}`, updatedFields);
 
           setOpenSnackbar(true);
-          setMessageSnackbar('Usuário atualizado com sucesso!');
-          setTypeSnackbar('success');
+          setMessageSnackbar("Usuário atualizado com sucesso!");
+          setTypeSnackbar("success");
 
           setUpdating(false);
         } else {
           setOpenSnackbar(true);
-          setMessageSnackbar('Dados não permitidos! Tente novamente.');
-          setTypeSnackbar('error');
+          setMessageSnackbar("Dados não permitidos! Tente novamente.");
+          setTypeSnackbar("error");
         }
-        
       } catch (error) {
         console.log(error);
 
         setOpenSnackbar(true);
-        setMessageSnackbar('Falha ao atualizar usuário.');
-        setTypeSnackbar('error');
+        setMessageSnackbar("Falha ao atualizar usuário.");
+        setTypeSnackbar("error");
 
         setUpdating(false);
       }
-      
+
       setLoading(false);
     }
   }
 
   function handleDelete(confirmation) {
-    if (updating) { //cancelar
+    if (updating) {
+      //cancelar
       setUpdating(false);
       setUserData(userDataOriginal);
-    }
-    else if (confirmation === true) { // excuir de verdade
+    } else if (confirmation === true) {
+      // excuir de verdade
       setDeleting(false);
-      alert("Excluindo usuário do banco de dados...")
-    }
-    else { // confirmar exclusão
+      alert("Excluindo usuário do banco de dados...");
+    } else {
+      // confirmar exclusão
       setDeleting(true);
     }
   }
 
-  // ---------------------------- // 
+  // ---------------------------- //
 
   if (!userData) {
     return (
       <React.Fragment>
         <CssBaseline />
         <div className={classes.root}>
-          <h1 className={classes.title}>
-            Detalhes de Usuário
-          </h1>
+          <h1 className={classes.title}>Detalhes de Usuário</h1>
           <Paper className={classes.containerForm} elevation={0}>
             <Typography variant="h5">Dados inválidos!</Typography>
           </Paper>
@@ -157,10 +166,7 @@ function AtualizacaoUsuario() {
   }
 
   const AreYouSure = () => (
-    <Dialog
-      open={deleting}
-      onClose={() => setDeleting(false)}
-    >
+    <Dialog open={deleting} onClose={() => setDeleting(false)}>
       <DialogTitle>Excluir usuário?</DialogTitle>
       <DialogContent>
         <DialogContentText>
@@ -170,10 +176,10 @@ function AtualizacaoUsuario() {
       <DialogActions>
         <Button color="primary" onClick={() => setDeleting(false)}>
           Cancelar
-          </Button>
+        </Button>
         <Button color="secondary" onClick={() => handleDelete(true)}>
           Excluir
-          </Button>
+        </Button>
       </DialogActions>
     </Dialog>
   );
@@ -182,7 +188,6 @@ function AtualizacaoUsuario() {
     <React.Fragment>
       <CssBaseline />
       <div className={classes.root}>
-
         <h1 className={classes.title}>
           {id === "me" ? "Seu Perfil" : "Detalhes do Usuário"}
         </h1>
@@ -190,58 +195,62 @@ function AtualizacaoUsuario() {
         <AreYouSure />
 
         <Paper className={classes.containerForm} elevation={0}>
-          { userData.type === 'PF' || id === "me"  ? 
-              <CadastroPF
-                formData={userData}
-                handleChangeInput={handleChangeInput}
-                mode={ updating ? 'edit' : 'view'}
-              />
-              :
-              (userData.type === 'PJ' ?
-                
-              <CadastroPJ
-                  formData={userData}
-                  handleChangeInput={handleChangeInput}
-                  mode={ updating? 'edit' : 'view'}
-                />
-                :
-                  (userData.type === "Funcionario" || userData.type === "Administrador" ?
-                      <CadastroFuncionario
-                        formData={userData}
-                        handleChangeInput={handleChangeInput}
-                        mode={ updating? 'edit' : 'view'}
-                      /> 
-                      :
-                      null
-                  )
-              )
-          }
-        
+          {userData.type === "PF" || id === "me" ? (
+            <CadastroPF
+              formData={userData}
+              handleChangeInput={handleChangeInput}
+              mode={updating ? "edit" : "view"}
+            />
+          ) : userData.type === "PJ" ? (
+            <CadastroPJ
+              formData={userData}
+              handleChangeInput={handleChangeInput}
+              mode={updating ? "edit" : "view"}
+            />
+          ) : userData.type === "Funcionario" ||
+            userData.type === "Administrador" ? (
+            <CadastroFuncionario
+              formData={userData}
+              handleChangeInput={handleChangeInput}
+              mode={updating ? "edit" : "view"}
+            />
+          ) : null}
+
           <Grid className={classes.centralizar} item xs={12}>
-            <Button variant="contained" color="primary" className={classes.btn}
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.btn}
               onClick={handleSubmit}
             >
-              {
-                updating ? "Salvar" : 
-                  loading ? (
-                    <CircularProgress color="secondary" />
-                  ) : "Editar"              
-              }
+              {updating ? (
+                "Salvar"
+              ) : loading ? (
+                <CircularProgress color="secondary" />
+              ) : (
+                "Editar"
+              )}
             </Button>
-      
-            {!(id === "me" && updating === false) &&
-              <Button variant="contained" color="secondary" className={classes.btn}
+
+            {!(id === "me" && updating === false) && (
+              <Button
+                variant="contained"
+                color="secondary"
+                className={classes.btn}
                 onClick={handleDelete}
               >
                 {updating ? "Cancelar" : "Excluir"}
               </Button>
-            }
+            )}
           </Grid>
         </Paper>
-
       </div>
 
-      <Snackbar open={openSnackbar} autoHideDuration={2000} onClose={() => setOpenSnackbar(false)}>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={2000}
+        onClose={() => setOpenSnackbar(false)}
+      >
         <MuiAlert
           onClose={() => setOpenSnackbar(false)}
           elevation={6}
@@ -251,7 +260,7 @@ function AtualizacaoUsuario() {
           {messageSnackbar}
         </MuiAlert>
       </Snackbar>
-    </React.Fragment >
+    </React.Fragment>
   );
 }
 
